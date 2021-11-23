a179213618556d93cfe496c4f87132e357fbf571
EDWARD SZILAGYI <edward.szilagyi@stud.ubbcluj.ro>
2021-11-23 15:42:34 +0200

    Add project files.

---
+++ Data/Szilagyi_Edward_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Szilagyi_Edward_Lab8.Models;
+
+namespace Szilagyi_Edward_Lab8.Data
+{
+    public class Szilagyi_Edward_Lab8Context : DbContext
+    {
+        public Szilagyi_Edward_Lab8Context (DbContextOptions<Szilagyi_Edward_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Szilagyi_Edward_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Szilagyi_Edward_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Szilagyi_Edward_Lab8.Models.Category> Category { get; set; }
+    }
+}
